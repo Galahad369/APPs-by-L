abdd03b5c59c95ecfbd7dd6f4b438ce89523386a
L0x3R <74096405+Galahad369@users.noreply.github.com>
2026-09-16 14:38:32 +0800

    Build Greater Art v1.11.1

--- greater-art/app/build.gradle.kts
+++ greater-art/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.local.listentomusic"
         minSdk = 26
         targetSdk = 37
-        versionCode = 76
-        versionName = "1.10.4"
+        versionCode = 77
+        versionName = "1.11.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
